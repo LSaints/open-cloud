fee1a4088327434094e93869b501466fd3b09bac
Mateus Leandro <132153413+LSaints@users.noreply.github.com>
2024-09-20 15:01:54 -0300

    feat: add route to login

--- backend/pkg/router/routes/route.go
+++ backend/pkg/router/routes/route.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"backend/internal/core/login"
 	"backend/internal/core/user"
 
 	"github.com/gorilla/mux"
@@ -8,6 +9,7 @@ import (
 
 func Setup(router *mux.Router) *mux.Router {
 	routes := user.UserRoutes
+	routes = append(routes, login.LoginRoutes)
 	for _, route := range routes {
 		router.HandleFunc(route.URI, route.Action).Methods(route.Method)
 	}
